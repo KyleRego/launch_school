7e25e4379979f074cebd04cdc6a5d8029295fa1c
Kyle Rego <regoky@outlook.com>
2022-01-10 14:15:50 -0500

    start rb_120 work

---
+++ rb_120/lesson1/zetcode/class_constant.rb
@@ -0,0 +1,5 @@
+class MMath
+  PI = 3.14159
+end
+
+puts MMath::PI
---
+++ rb_120/lesson1/zetcode/inheritance.rb
@@ -0,0 +1,17 @@
+class Being
+
+  def initialize
+    puts "Being class created"
+  end
+end
+
+class Human < Being
+
+  def initialize
+    super
+    puts "Human class created"
+  end
+end
+
+Being.new
+Human.new
