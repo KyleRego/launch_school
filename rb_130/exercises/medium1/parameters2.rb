85639264e9d2fcd7f871e35c76151b503398da46
Kyle Rego <regoky@outlook.com>
2022-02-08 09:56:56 -0500

    Medium 1 exercises

--- rb_130/exercises/medium1/parameters2.rb
+++ rb_130/exercises/medium1/parameters2.rb
@@ -2,9 +2,6 @@ def my_method(array)
   yield(array)
 end
 
-arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
-value = my_method(arr) do |arr|
-  first, second, *raptors = arr
+my_method do |_, _, *raptors|
   puts raptors
 end
