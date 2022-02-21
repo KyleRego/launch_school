6a491bbfb4812d42a923f190cbaf42e23e76c107
Kyle Rego <regoky@outlook.com>
2022-02-21 12:13:03 -0500

    Initial work on simple HTTP server

---
+++ ls_170/simple_http_server/http_server.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/bash
+
+function server () {
+  while true
+  do
+    read method path version
+    if [[ $method = 'GET' ]]
+    then
+      if [[ -f "./www$path" ]]
+      then
+        echo -en "HTTP/1.1 200 OK\r\n\r\n"
+        cat "./www$path"
+      else
+        echo -en "HTTP/1.1 404 Not Found\r\n\r\n"
+      fi
+    else
+      echo -en 'HTTP/1.1 400 Bad Request\r\n\r\n'
+    fi
+  done
+}
+
+coproc SERVER_PROCESS { server; }
+
+netcat -lv 2345 <&${SERVER_PROCESS[0]} >&${SERVER_PROCESS[1]}
