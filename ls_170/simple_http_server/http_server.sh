42557605b6126e2932713346c5eacb277dc60481
Kyle Rego <regoky@outlook.com>
2022-02-21 12:27:08 -0500

    Finish simple HTTP server

--- ls_170/simple_http_server/http_server.sh
+++ ls_170/simple_http_server/http_server.sh
@@ -3,22 +3,39 @@
 function server () {
   while true
   do
-    read method path version
+    message_arr=()
+    check=true
+    while $check
+    do
+      read line
+      message_arr+=($line)
+      if [[ "${#line}" -eq 1 ]]
+      then
+        check=false
+      fi
+    done
+    method=${message_arr[0]}
+    path=${message_arr[1]}
     if [[ $method = 'GET' ]]
     then
       if [[ -f "./www$path" ]]
       then
-        echo -en "HTTP/1.1 200 OK\r\n\r\n"
+        echo -en "HTTP/1.1 200 OK\r\n"
+        echo -en "Content-Type: text/html; charset=utf-8\r\n"
+        content_length=`wc -c < "./www$path"`
+        echo -en "Content-Length: $content_length\r\n\r\n"
         cat "./www$path"
       else
-        echo -en "HTTP/1.1 404 Not Found\r\n\r\n"
+        echo -en "HTTP/1.1 404 Not Found\r\n"
+        echo -en "Content-Length: 0\r\n\r\n"
       fi
     else
-      echo -en 'HTTP/1.1 400 Bad Request\r\n\r\n'
+      echo -en 'HTTP/1.1 400 Bad Request\r\n'
+      echo -en "Content-Length: 0\r\n\r\n"
     fi
   done
 }
 
 coproc SERVER_PROCESS { server; }
 
-netcat -lv 2345 <&${SERVER_PROCESS[0]} >&${SERVER_PROCESS[1]}
+netcat -lkv 2345 <&${SERVER_PROCESS[0]} >&${SERVER_PROCESS[1]}
